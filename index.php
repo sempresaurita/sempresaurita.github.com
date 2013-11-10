e7311afca18899703c1aa2a973b479f3d5c0e86b
Antonella Tezza <antonellatezza@gmail.com>
2013-11-10 18:09:00 +0100

    updated favicon

--- index.php
+++ index.php
@@ -31,10 +31,24 @@
 	
 	<!-- Favicons
 	================================================== -->
-	<link rel="shortcut icon" href="images/favicon.ico">
-	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="images/favicons/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="images/favicons/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="images/favicons/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="images/favicons/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="60x60" href="images/favicons/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="images/favicons/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="images/favicons/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="images/favicons/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="images/favicons/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" type="image/png" href="images/favicons/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="images/favicons/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/png" href="images/favicons/favicon-160x160.png" sizes="160x160" />
+	<meta name="msapplication-TileColor" content="#da532c" />
+	<meta name="msapplication-TileImage" content="images/favicons/mstile-144x144.png" />
+	<meta name="msapplication-square70x70logo" content="images/favicons/mstile-70x70.png" />
+	<meta name="msapplication-square150x150logo" content="images/favicons/mstile-150x150.png" />
+	<meta name="msapplication-wide310x150logo" content="images/favicons/mstile-310x150.png" />
+	<meta name="msapplication-square310x310logo" content="images/favicons/mstile-310x310.png" />
 </head>
 
 <body lang="en">
