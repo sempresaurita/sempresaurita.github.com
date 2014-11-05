522f9537532bf5fa8ccb7da1ad0ea31f6379e5db
Martins Grunskis <martins@grunskis.com>
2014-11-05 09:30:59 +0100

    Fix overlays and allow to have many of them

--- relaunch/js/overlay.js
+++ relaunch/js/overlay.js
@@ -1,45 +1,53 @@
 (function() {
-	var container = ( 'div.container' ),
-		triggerBttn = ( '#trigger-overlay' ),
-		overlay = ( 'div.overlay' ),
-		closeBttn = ( 'button.overlay-close' );
-
-		transEndEventNames = {
-			'WebkitTransition': 'webkitTransitionEnd',
-			'MozTransition': 'transitionend',
-			'OTransition': 'oTransitionEnd',
-			'msTransition': 'MSTransitionEnd',
-			'transition': 'transitionend'
-		},
-		
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		support = { transitions : Modernizr.csstransitions };
-
-	function toggleOverlay() {
-		if( classie.has( overlay, 'open' ) ) {
-			classie.remove( overlay, 'open' );
-			classie.remove( container, 'overlay-open' );
-			classie.add( overlay, 'close' );
-			var onEndTransitionFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.propertyName !== 'visibility' ) return;
-					this.removeEventListener( transEndEventName, onEndTransitionFn );
-				}
-				classie.remove( overlay, 'close' );
-			};
-			if( support.transitions ) {
-				overlay.addEventListener( transEndEventName, onEndTransitionFn );
-			}
-			else {
-				onEndTransitionFn();
-			}
+    function toggleOverlay(overlay) {
+	if( classie.has( overlay, 'open' ) ) {
+	    classie.remove( overlay, 'open' );
+	    classie.remove( container, 'overlay-open' );
+	    classie.add( overlay, 'close' );
+	    var onEndTransitionFn = function( ev ) {
+		if( support.transitions ) {
+		    if( ev.propertyName !== 'visibility' ) return;
+		    this.removeEventListener( transEndEventName, onEndTransitionFn );
 		}
-		else if( !classie.has( overlay, 'close' ) ) {
-			classie.add( overlay, 'open' );
-			classie.add( container, 'overlay-open' );
-		}
+		classie.remove( overlay, 'close' );
+	    };
+	    if( support.transitions ) {
+		overlay.addEventListener( transEndEventName, onEndTransitionFn );
+	    }
+	    else {
+		onEndTransitionFn();
+	    }
 	}
+	else if( !classie.has( overlay, 'close' ) ) {
+	    classie.add( overlay, 'open' );
+	    classie.add( container, 'overlay-open' );
+	}
+    }
+    
+    var container = document.querySelector('div.container');
 
-	triggerBttn.addEventListener( 'click', toggleOverlay );
-	closeBttn.addEventListener( 'click', toggleOverlay );
+    transEndEventNames = {
+	'WebkitTransition': 'webkitTransitionEnd',
+	'MozTransition': 'transitionend',
+	'OTransition': 'oTransitionEnd',
+	'msTransition': 'MSTransitionEnd',
+	'transition': 'transitionend'
+    },
+    
+    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+    support = { transitions : Modernizr.csstransitions };
+
+    var overlayNames = ['foo', 'bar']; // , 'baz', 'qux', 'quux'
+    overlayNames.forEach(function (name) {
+        var triggerBttn = document.querySelector('#trigger-overlay-' + name),
+            overlay = document.querySelector('div.overlay-' + name),
+            closeBttn = document.querySelector('button.overlay-close-' + name);
+     
+        triggerBttn.addEventListener('click', function () {
+            toggleOverlay(overlay);
+        });
+        closeBttn.addEventListener('click', function () {
+            toggleOverlay(overlay);
+        });
+    });
 })();
